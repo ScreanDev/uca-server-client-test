df3383d7a719c0d603bd073f67f5e0b883ff8fb3
Lucas Pausé-Chapuis <lucas.pause-chapuis@universite-paris-saclay.fr>
2026-04-14 09:49:44 +0200

    Display cards list in the client

--- runServer.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo LANCEMENT DU SERVEUR
-echo --------------------------
-
-start "Serveur Python" cmd /k "python serveur.py"
-start "Tunnel ngrok" cmd /k "ngrok http --domain=nontabular-nonexponentially-shannan.ngrok-free.dev 8080"
-
-exit
